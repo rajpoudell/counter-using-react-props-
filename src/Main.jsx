7f8c13623e350f2a6e894c7642b67328d67a127d
Raj Poudel <raj.poudeel@gmail.com>
2023-05-17 21:24:18 +0545

    Main file has all property with the project

---
+++ src/Main.jsx
@@ -0,0 +1,23 @@
+import React,{useState} from "react";
+
+export default function Main() {
+
+const [count,setcount]=useState(0);
+function add(){
+    setcount(count+1)
+}
+function substract(){
+    setcount(count-1)
+}
+
+
+
+
+  return (
+    <div className="main">
+      <button className="minus common" onClick={substract}>-</button>
+      <div className="count common">{count}</div>
+      <button className="add common" onClick={add}>+</button>
+    </div>
+  );
+}
